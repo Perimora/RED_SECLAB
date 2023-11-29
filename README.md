de12fed63c2870b53f5dbdf3523c3e83a12f2061
Christoph Huy <christoph.huy@campus-tu-berlin.de>
2023-11-28 20:04:43 -0500

    hopefully finished soon

---
+++ README.md
@@ -0,0 +1,4 @@
+## Build Instructions
+
+To build the game binary 'RED' simply run `./build.sh`.
+The output will be in the project directory as 'RED.txt'.
---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+make 
+
+input_file="red.bin"
+output_file="RED.txt"
+
+# Encode binary file to base64 and save the result in the output file
+base64 "$input_file" > "./out/$output_file"
+
+make clean
+
+echo "Conversion complete. Base64 output saved to '$output_file'."
